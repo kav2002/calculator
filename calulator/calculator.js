7fe6fdd35480dc3e197d2558b9a2adb4d3608376
kav2002 <kavyakoudi30@gmail.com>
2023-06-08 20:17:04 +0530

    calci

---
+++ calulator/calculator.js
@@ -0,0 +1,14 @@
+
+function appendToDisplay(value) {
+  document.getElementById("display").value += value;
+}
+
+function calculate() {
+  var display = document.getElementById("display");
+  var result = eval(display.value);
+  display.value = result;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
